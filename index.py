1d4f96849896e5d4f6225ead4197ff529ebe2c94
David Velasquez <drvelasquezr@outlook.com>
2023-05-24 19:55:43 -0500

    change index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print ("Bienvenidos DevOps For Beginners")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>Bienvenidos</h1>'
+
+if __name__ == '__main__':
+    app.run()
